96bca13a296d5b849de245e6a9c1166bd50566e5
jakej985-rgb <jakej985@gmail.com>
2026-09-11 15:22:09 -0600

    chore: rebrand widget preview app and update paths for Red Music Locker

--- app/.widget_preview/lib/src/dtd/dtd_connection_info.dart
+++ app/.widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: implementation_imports
 
-const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:33105/cjxK7ho0cpk=';
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:36495/1_xMl-621WA=';
 const String kWidgetPreviewService =
-    'widget-preview-3088a439-a982-4ec7-a7cc-f564721738c5';
+    'widget-preview-c75793ff-e6a6-4005-9644-516f88ab7213';
 const String kWidgetPreviewScaffoldStream =
-    'WidgetPreviewScaffold-3088a439-a982-4ec7-a7cc-f564721738c5';
-const String kProjectRootPath = r'/home/m3tal/Workspaces/ytmusic_sync/app';
+    'WidgetPreviewScaffold-c75793ff-e6a6-4005-9644-516f88ab7213';
+const String kProjectRootPath = r'/home/m3tal/Workspaces/red-music-locker/app';
